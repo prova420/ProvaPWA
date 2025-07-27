a67bbf1c4f2968d78c9cc9b4c927394e8da84fa2
prova420 <enzo.zoldan@protonmail.com>
2025-07-27 02:33:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,30 @@
-const CACHE_NAME = 'magazzino-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
-  'icons/icon-512x512.png'
+const CACHE_NAME = 'magazzino-v2';
+const ASSETS_TO_CACHE = [
+  './',                   // Pagina principale
+  './index.html',         // File HTML principale
+  './manifest.json',      // Manifest
+  './icons/icon-192.png', // Percorsi corretti delle icone
+  './icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Cache aperta');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .catch(error => {
+        console.error('Errore cache.addAll:', error);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
